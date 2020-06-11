923c3455df9d607c586601d3542994d341662245
ZOI <zoi@zoizoi.net>
2020-06-11 18:58:41 +0900

    サウンド、カウントダウン追加

--- src/main/java/net/zoizoi/plugin/werewolf/Command/SubCommand/SubCommands/startSubCommand.java
+++ src/main/java/net/zoizoi/plugin/werewolf/Command/SubCommand/SubCommands/startSubCommand.java
@@ -3,8 +3,10 @@ package net.zoizoi.plugin.werewolf.Command.SubCommand.SubCommands;
 import net.zoizoi.plugin.werewolf.Command.SubCommand.SubCommandMaster;
 import net.zoizoi.plugin.werewolf.Game.GameManager;
 import net.zoizoi.plugin.werewolf.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Item;
@@ -14,6 +16,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -24,104 +27,160 @@ import java.util.Map;
 
 public class startSubCommand {
   public boolean OnCommand(Player player, Command command, String label, String[] args, Main plugin, GameManager gameManager, int GameID) {
-    gameManager.getGame(GameID).Start(plugin);
-    // ステージへテレポート
-    Location gameStage = new Location(player.getWorld(),
-      plugin.config.getDouble("Location.gameStage.x"),
-      plugin.config.getDouble("Location.gameStage.y"),
-      plugin.config.getDouble("Location.gameStage.z"));
-    // ゲームに参加しているプレイヤー全員に対して
-    for (Player p : gameManager.getGame(GameID).getPlayers().keySet()) {
-      Inventory inventory = p.getInventory();
-      inventory.clear();
-      // 弓
-      ItemStack SuperBow = new ItemStack(Material.BOW);
-      SuperBow.addUnsafeEnchantment(Enchantment.ARROW_DAMAGE, 10000);
-      SuperBow.addUnsafeEnchantment(Enchantment.ARROW_INFINITE, 10000);
-      ItemMeta SuperBowMeta = SuperBow.getItemMeta();
-      SuperBowMeta.setDisplayName("一撃必殺の弓");
-      SuperBow.setItemMeta(SuperBowMeta); // Set Meta
-      inventory.addItem(SuperBow);
-      // 矢
-      ItemStack Arrow = new ItemStack(Material.ARROW, 16);
-      inventory.addItem(Arrow);
-      // ステーキ
-      ItemStack Steak = new ItemStack(Material.COOKED_BEEF, 32);
-      inventory.addItem(Steak);
-      // ランダムアイテム配布
-      List<ItemStack> RundomItems = new ArrayList<ItemStack>();
-      // ちくわ
-      ItemStack Tikuwa = new ItemStack(Material.STICK,1);
-      ItemMeta TikuwaMeta = Tikuwa.getItemMeta();
-      TikuwaMeta.setDisplayName("ちくわ");
-      Tikuwa.setItemMeta(TikuwaMeta); // Set Meta
-      RundomItems.add(Tikuwa);
-      // 跳躍5ポーション
-      ItemStack Potion_JUMP_5 = new ItemStack(Material.POTION,1);
-      PotionMeta Potion_JUMP_5_Meta = (PotionMeta) Potion_JUMP_5.getItemMeta();
-      Potion_JUMP_5_Meta.setDisplayName("跳躍力上昇のポーション");
-      Potion_JUMP_5_Meta.addCustomEffect(new PotionEffect(PotionEffectType.JUMP, 600, 4), true);
-      Potion_JUMP_5.setItemMeta(Potion_JUMP_5_Meta); // Set Meta
-      RundomItems.add(Potion_JUMP_5);
-      // 移動速度上昇5
-      ItemStack Potion_SPEED_5 = new ItemStack(Material.POTION,1);
-      PotionMeta Potion_SPEED_5_Meta = (PotionMeta) Potion_SPEED_5.getItemMeta();
-      Potion_SPEED_5_Meta.setDisplayName("移動速度上昇のポーション");
-      Potion_SPEED_5_Meta.addCustomEffect(new PotionEffect(PotionEffectType.SPEED, 600, 4), true);
-      Potion_SPEED_5.setItemMeta(Potion_SPEED_5_Meta); // Set Meta
-      RundomItems.add(Potion_SPEED_5);
-      // 一撃必殺ピッケル
-      ItemStack SuperPickaxe = new ItemStack(Material.STONE_PICKAXE,1);
-      SuperPickaxe.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 10000);
-      ItemMeta SuperPickaxeMeta = SuperPickaxe.getItemMeta();
-      SuperPickaxeMeta.setDisplayName("一撃必殺のピッケル");
-      if (SuperPickaxeMeta instanceof Damageable) {
-        ((Damageable) SuperPickaxeMeta).setDamage(10);
+    // 0秒後
+    plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+      public void run() {
+        for (Player p : gameManager.getGame(GameID).getPlayers().keySet()) {
+          p.playSound(p.getLocation(), Sound.BLOCK_LEVER_CLICK, 1, 1);
+          p.sendMessage("五秒前...");
+          p.sendTitle("五秒前", "", 10, 20, 10);
+        }
       }
-      SuperPickaxe.setItemMeta(SuperPickaxeMeta); // Set Meta
-      RundomItems.add(SuperPickaxe);
-      // 不死のトーテム
-      ItemStack Totem = new ItemStack(Material.TOTEM_OF_UNDYING,1);
-      RundomItems.add(Totem);
-      // 透明ポーション
-      ItemStack Potion_Invisibility = new ItemStack(Material.POTION,1);
-      PotionMeta Potion_Invisibility_Meta = (PotionMeta) Potion_Invisibility.getItemMeta();
-      Potion_Invisibility_Meta.setDisplayName("透明化のポーション");
-      Potion_Invisibility_Meta.addCustomEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 600, 1), true);
-      Potion_Invisibility.setItemMeta(Potion_Invisibility_Meta); // Set Meta
-      RundomItems.add(Potion_Invisibility);
-      // 一撃残留ポーション
-      ItemStack Potion_Damage_Lingering = new ItemStack(Material.LINGERING_POTION,1);
-      PotionMeta Potion_Damage_Lingering_Meta = (PotionMeta) Potion_Damage_Lingering.getItemMeta();
-      Potion_Damage_Lingering_Meta.setDisplayName("一撃必殺の残留ポーション");
-      Potion_Damage_Lingering_Meta.addCustomEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE,600,1),true);
-      Potion_Damage_Lingering.setItemMeta(Potion_Damage_Lingering_Meta); // Set Meta
-      RundomItems.add(Potion_Damage_Lingering);
-      // すごいちくわ
-      ItemStack SuperTikuwa = new ItemStack(Material.STICK,1);
-      SuperTikuwa.addUnsafeEnchantment(Enchantment.KNOCKBACK,2);
-      ItemMeta SuperTikuwaMeta = SuperTikuwa.getItemMeta();
-      SuperTikuwaMeta.setDisplayName("すごいちくわ");
-      SuperTikuwa.setItemMeta(SuperTikuwaMeta); // Set Meta
-      RundomItems.add(SuperTikuwa);
-      // 盲目残留ポーション(x3)
-      ItemStack Potion_Blindness_Lingering = new ItemStack(Material.LINGERING_POTION,3);
-      PotionMeta Potion_Blindness_Lingering_Meta = (PotionMeta) Potion_Blindness_Lingering.getItemMeta();
-      Potion_Blindness_Lingering_Meta.setDisplayName("盲目の残留ポーション");
-      Potion_Blindness_Lingering_Meta.addCustomEffect(new PotionEffect(PotionEffectType.BLINDNESS,600,1),true);
-      Potion_Blindness_Lingering.setItemMeta(Potion_Blindness_Lingering_Meta); // Set Meta
-      RundomItems.add(Potion_Blindness_Lingering);
-      //
+    }, (0 * 20));
+    // 1秒後
+    plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+      public void run() {
+        for (Player p : gameManager.getGame(GameID).getPlayers().keySet()) {
+          p.playSound(p.getLocation(), Sound.BLOCK_LEVER_CLICK, 1, 1);
+          p.sendMessage("四秒前...");
+          p.sendTitle("四秒前", "", 10, 20, 10);
+        }
+      }
+    }, (1 * 20));
+    // 2秒後
+    Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+      public void run() {
+        for (Player p : gameManager.getGame(GameID).getPlayers().keySet()) {
+          p.playSound(p.getLocation(), Sound.BLOCK_LEVER_CLICK, 1, 1);
+          p.sendMessage("三秒前...");
+          p.sendTitle("三秒前", "", 10, 20, 10);
+        }
+      }
+    }, (2 * 20));
+    // 3秒後
+    Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+      public void run() {
+        for (Player p : gameManager.getGame(GameID).getPlayers().keySet()) {
+          p.playSound(p.getLocation(), Sound.BLOCK_LEVER_CLICK, 1, 1);
+          p.sendMessage("二秒前...");
+          p.sendTitle("二秒前", "", 10, 20, 10);
+        }
+      }
+    }, (3 * 20));
+    // 4秒後
+    Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+      public void run() {
+        for (Player p : gameManager.getGame(GameID).getPlayers().keySet()) {
+          p.playSound(p.getLocation(), Sound.BLOCK_LEVER_CLICK, 1, 1);
+          p.sendMessage("一秒前...");
+          p.sendTitle("一秒前", "", 10, 20, 10);
+        }
+      }
+    }, (4 * 20));
+    // 5秒後
+    Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+      public void run() {
+        gameManager.getGame(GameID).Start(plugin);
+        // ステージへテレポート
+        Location gameStage = new Location(player.getWorld(),
+          plugin.config.getDouble("Location.gameStage.x"),
+          plugin.config.getDouble("Location.gameStage.y"),
+          plugin.config.getDouble("Location.gameStage.z"));
+        // ゲームに参加しているプレイヤー全員に対して
+        for (Player p : gameManager.getGame(GameID).getPlayers().keySet()) {
+          Inventory inventory = p.getInventory();
+          inventory.clear();
+          // 弓
+          ItemStack SuperBow = new ItemStack(Material.BOW);
+          SuperBow.addUnsafeEnchantment(Enchantment.ARROW_DAMAGE, 10000);
+          SuperBow.addUnsafeEnchantment(Enchantment.ARROW_INFINITE, 10000);
+          ItemMeta SuperBowMeta = SuperBow.getItemMeta();
+          SuperBowMeta.setDisplayName("一撃必殺の弓");
+          SuperBow.setItemMeta(SuperBowMeta); // Set Meta
+          inventory.addItem(SuperBow);
+          // 矢
+          ItemStack Arrow = new ItemStack(Material.ARROW, 16);
+          inventory.addItem(Arrow);
+          // ステーキ
+          ItemStack Steak = new ItemStack(Material.COOKED_BEEF, 32);
+          inventory.addItem(Steak);
+          // ランダムアイテム配布
+          List<ItemStack> RundomItems = new ArrayList<ItemStack>();
+          // ちくわ
+          ItemStack Tikuwa = new ItemStack(Material.STICK, 1);
+          ItemMeta TikuwaMeta = Tikuwa.getItemMeta();
+          TikuwaMeta.setDisplayName("ちくわ");
+          Tikuwa.setItemMeta(TikuwaMeta); // Set Meta
+          RundomItems.add(Tikuwa);
+          // 跳躍5ポーション
+          ItemStack Potion_JUMP_5 = new ItemStack(Material.POTION, 1);
+          PotionMeta Potion_JUMP_5_Meta = (PotionMeta) Potion_JUMP_5.getItemMeta();
+          Potion_JUMP_5_Meta.setDisplayName("跳躍力上昇のポーション");
+          Potion_JUMP_5_Meta.addCustomEffect(new PotionEffect(PotionEffectType.JUMP, 600, 4), true);
+          Potion_JUMP_5.setItemMeta(Potion_JUMP_5_Meta); // Set Meta
+          RundomItems.add(Potion_JUMP_5);
+          // 移動速度上昇5
+          ItemStack Potion_SPEED_5 = new ItemStack(Material.POTION, 1);
+          PotionMeta Potion_SPEED_5_Meta = (PotionMeta) Potion_SPEED_5.getItemMeta();
+          Potion_SPEED_5_Meta.setDisplayName("移動速度上昇のポーション");
+          Potion_SPEED_5_Meta.addCustomEffect(new PotionEffect(PotionEffectType.SPEED, 600, 4), true);
+          Potion_SPEED_5.setItemMeta(Potion_SPEED_5_Meta); // Set Meta
+          RundomItems.add(Potion_SPEED_5);
+          // 一撃必殺ピッケル
+          ItemStack SuperPickaxe = new ItemStack(Material.STONE_PICKAXE, 1);
+          SuperPickaxe.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 10000);
+          ItemMeta SuperPickaxeMeta = SuperPickaxe.getItemMeta();
+          SuperPickaxeMeta.setDisplayName("一撃必殺のピッケル");
+          if (SuperPickaxeMeta instanceof Damageable) {
+            ((Damageable) SuperPickaxeMeta).setDamage(10);
+          }
+          SuperPickaxe.setItemMeta(SuperPickaxeMeta); // Set Meta
+          RundomItems.add(SuperPickaxe);
+          // 不死のトーテム
+          ItemStack Totem = new ItemStack(Material.TOTEM_OF_UNDYING, 1);
+          RundomItems.add(Totem);
+          // 透明ポーション
+          ItemStack Potion_Invisibility = new ItemStack(Material.POTION, 1);
+          PotionMeta Potion_Invisibility_Meta = (PotionMeta) Potion_Invisibility.getItemMeta();
+          Potion_Invisibility_Meta.setDisplayName("透明化のポーション");
+          Potion_Invisibility_Meta.addCustomEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 600, 1), true);
+          Potion_Invisibility.setItemMeta(Potion_Invisibility_Meta); // Set Meta
+          RundomItems.add(Potion_Invisibility);
+          // 一撃残留ポーション
+          ItemStack Potion_Damage_Lingering = new ItemStack(Material.LINGERING_POTION, 1);
+          PotionMeta Potion_Damage_Lingering_Meta = (PotionMeta) Potion_Damage_Lingering.getItemMeta();
+          Potion_Damage_Lingering_Meta.setDisplayName("一撃必殺の残留ポーション");
+          Potion_Damage_Lingering_Meta.addCustomEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 600, 1), true);
+          Potion_Damage_Lingering.setItemMeta(Potion_Damage_Lingering_Meta); // Set Meta
+          RundomItems.add(Potion_Damage_Lingering);
+          // すごいちくわ
+          ItemStack SuperTikuwa = new ItemStack(Material.STICK, 1);
+          SuperTikuwa.addUnsafeEnchantment(Enchantment.KNOCKBACK, 2);
+          ItemMeta SuperTikuwaMeta = SuperTikuwa.getItemMeta();
+          SuperTikuwaMeta.setDisplayName("すごいちくわ");
+          SuperTikuwa.setItemMeta(SuperTikuwaMeta); // Set Meta
+          RundomItems.add(SuperTikuwa);
+          // 盲目残留ポーション(x3)
+          ItemStack Potion_Blindness_Lingering = new ItemStack(Material.LINGERING_POTION, 3);
+          PotionMeta Potion_Blindness_Lingering_Meta = (PotionMeta) Potion_Blindness_Lingering.getItemMeta();
+          Potion_Blindness_Lingering_Meta.setDisplayName("盲目の残留ポーション");
+          Potion_Blindness_Lingering_Meta.addCustomEffect(new PotionEffect(PotionEffectType.BLINDNESS, 600, 1), true);
+          Potion_Blindness_Lingering.setItemMeta(Potion_Blindness_Lingering_Meta); // Set Meta
+          RundomItems.add(Potion_Blindness_Lingering);
+          //
 
-      List<Player> shuffledPlayerList = new ArrayList<Player>(gameManager.getGame(GameID).getPlayers().keySet());
-      Collections.shuffle(shuffledPlayerList);
-      for (int i = 0; i < shuffledPlayerList.size(); i++) {
-        shuffledPlayerList.get(i).getInventory().addItem(RundomItems.get(i));
+          List<Player> shuffledPlayerList = new ArrayList<Player>(gameManager.getGame(GameID).getPlayers().keySet());
+          Collections.shuffle(shuffledPlayerList);
+          for (int i = 0; i < shuffledPlayerList.size(); i++) {
+            shuffledPlayerList.get(i).getInventory().addItem(RundomItems.get(i));
+          }
+          //
+          p.sendTitle("開始", "ゲームが開始されました", 10, 50, 10);
+          p.teleport(gameStage);
+          p.playSound(p.getLocation(), Sound.BLOCK_ANVIL_USE, 1, 1);
+        }
       }
-      //
-      p.sendTitle("開始", "ゲームが開始されました", 10, 50, 10);
-      p.teleport(gameStage);
-    }
+    }, (5 * 20));
     return true;
   }
 }
--- src/main/java/net/zoizoi/plugin/werewolf/Game/GameJudge.java
+++ src/main/java/net/zoizoi/plugin/werewolf/Game/GameJudge.java
@@ -2,13 +2,17 @@ package net.zoizoi.plugin.werewolf.Game;
 
 import net.zoizoi.plugin.werewolf.Command.SubCommand.SubCommandMaster;
 import net.zoizoi.plugin.werewolf.Main;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.meta.FireworkMeta;
 
 public class GameJudge implements Listener {
   private Main plugin;
@@ -23,34 +27,53 @@ public class GameJudge implements Listener {
   public void onDeath(PlayerDeathEvent e) {
     this.gameManager = SubCommandMaster.gameManager;
     this.GameID = SubCommandMaster.GameID;
-    plugin.getLogger().info("Stop");
-    Player death = e.getEntity();
-    if (gameManager.getGame(GameID).PlayerDie(death)) {
-      gameManager.getGame(GameID).Stop();
-      for (Player player : gameManager.getGame(GameID).getPlayers().keySet()) {
-        player.getInventory().clear();
-        player.setGameMode(GameMode.SPECTATOR);
-        plugin.getLogger().info(plugin.config.getString("japanese.camp." + gameManager.getGame(GameID).getResult()));
-        player.sendTitle(plugin.config.getString("japanese.camp." + gameManager.getGame(GameID).getResult()) + "の勝利", "", 10, 250, 10);
-        player.sendMessage("+-----------+");
-        player.sendMessage("| "+plugin.config.getString("japanese.camp." + gameManager.getGame(GameID).getResult()) + "の勝利 |");
-        player.sendMessage("+-----------+");
-        player.sendMessage("");
-        player.sendMessage("今回の役職配分");
-        player.sendMessage("");
-        for (Player keyPlayer : gameManager.getGame(GameID).getPlayers().keySet()){
-          GamePlayer gamePlayer = gameManager.getGame(GameID).getPlayers().get(keyPlayer);
-          player.sendMessage(keyPlayer.getName()+" : "+gamePlayer.getJob().getJobNameJapanese());
+    if (gameManager.getGame(GameID).isRunning) {
+      plugin.getLogger().info("Stop");
+      Player death = e.getEntity();
+      if (gameManager.getGame(GameID).PlayerDie(death)) {
+        gameManager.getGame(GameID).Stop();
+        for (Player player : gameManager.getGame(GameID).getPlayers().keySet()) {
+          player.getInventory().clear();
+          player.setGameMode(GameMode.SPECTATOR);
+          plugin.getLogger().info(plugin.config.getString("japanese.camp." + gameManager.getGame(GameID).getResult()));
+          player.sendTitle(plugin.config.getString("japanese.camp." + gameManager.getGame(GameID).getResult()) + "の勝利", "", 10, 250, 10);
+          player.sendMessage("+-----------+");
+          player.sendMessage("| " + plugin.config.getString("japanese.camp." + gameManager.getGame(GameID).getResult()) + "の勝利 |");
+          player.sendMessage("+-----------+");
+          player.sendMessage("");
+          player.sendMessage("今回の役職配分");
+          player.sendMessage("");
+          for (Player keyPlayer : gameManager.getGame(GameID).getPlayers().keySet()) {
+            GamePlayer gamePlayer = gameManager.getGame(GameID).getPlayers().get(keyPlayer);
+            player.sendMessage(keyPlayer.getName() + " : " + gamePlayer.getJob().getJobNameJapanese());
+          }
+          player.sendMessage("");
+          // 花火を打ち上げる
+          /*
+          for (int i = 0; i < 10; i++) {
+            Firework firework = player.getWorld().spawn(player.getLocation(), Firework.class);
+            FireworkMeta fireworkMeta = firework.getFireworkMeta();
+            FireworkEffect.Builder effect = FireworkEffect.builder();
+            effect.with(FireworkEffect.Type.BALL_LARGE);
+            effect.withColor(Color.YELLOW);
+            effect.withFade(Color.RED);
+            effect.flicker(true);
+            effect.trail(true);
+            fireworkMeta.addEffect(effect.build());
+            fireworkMeta.setPower(1);
+            firework.setFireworkMeta(fireworkMeta);
+          }
+          //*/
         }
+      } else {
+        Location quitLobby = new Location(death.getWorld(),
+          plugin.config.getDouble("Location.quitLobby.x"),
+          plugin.config.getDouble("Location.quitLobby.y"),
+          plugin.config.getDouble("Location.quitLobby.z"));
+        death.teleport(quitLobby);
+        death.sendMessage("あなたは死にました");
+        death.sendMessage("DiscordのVCを切ってください");
       }
-    } else {
-      Location quitLobby = new Location(death.getWorld(),
-        plugin.config.getDouble("Location.quitLobby.x"),
-        plugin.config.getDouble("Location.quitLobby.y"),
-        plugin.config.getDouble("Location.quitLobby.z"));
-      death.teleport(quitLobby);
-      death.sendMessage("あなたは死にました");
-      death.sendMessage("DiscordのVCを切ってください");
     }
   }
 }
