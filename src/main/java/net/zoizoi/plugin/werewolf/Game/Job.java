6c4f80227853e3ffff42614eb239d2cf56107c6f
zoi <zoi@zoizoi.net>
2020-08-12 22:08:01 +0900

    JobのisUsedを消した。多分もう仕事はできませんバグは治った?

--- src/main/java/net/zoizoi/plugin/werewolf/Game/Job.java
+++ src/main/java/net/zoizoi/plugin/werewolf/Game/Job.java
@@ -3,78 +3,81 @@ package net.zoizoi.plugin.werewolf.Game;
 import net.zoizoi.plugin.werewolf.Main;
 
 public class Job {
-  Main plugin;
-  private String jobName;
-  private String camp;
-  private boolean isUsed;
+    Main plugin;
+    private String jobName;
+    private String camp;
+    // private boolean isUsed;
 
-  public Job(Main plugin, String jobName) {
-    this.plugin = plugin;
-    this.jobName = jobName;
-    this.isUsed = true;
-    switch (jobName) {
-      case "Citizen":     // 市民
-      case "Prophet":     // 予言者
-      case "Necromancer": // 霊媒師
-        camp = "Village";
-        break;
-      case "Werewolf":    // 人狼
-      case "Betrayer":    // 狂人
-        camp = "Wolf";
-        break;
-      default:
-        camp = "None";
-        break;
-    }
-  }
-
-  public String getJobName() {
-    if (jobName != null) {
-      return jobName;
-    } else {
-      return "";
-    }
-  }
-
-  public String getJobNameJapanese() {
-    return plugin.config.getString("japanese.jobs." + jobName);
-  }
-
-
-  public String getCamp() {
-    return this.camp;
-  }
-
-  public String Work(GamePlayer gamePlayer) {
-    if (!isUsed) {
-      isUsed = true;
-      if (jobName == "Prophet") {
-        if (gamePlayer.getLife()) {
-          if (gamePlayer.getJob().jobName == "Werewolf") {
-            return "この人は人狼です";
-          } else {
-            return "この人は人狼ではありません";
-          }
-        } else {
-          return "この人は死んでいます";
+    public Job(Main plugin, String jobName) {
+        this.plugin = plugin;
+        this.jobName = jobName;
+        // this.isUsed = true;
+        switch (jobName) {
+            case "Citizen":     // 市民
+            case "Prophet":     // 予言者
+            case "Necromancer": // 霊媒師
+                camp = "Village";
+                break;
+            case "Werewolf":    // 人狼
+            case "Betrayer":    // 狂人
+                camp = "Wolf";
+                break;
+            default:
+                camp = "None";
+                break;
         }
-      } else if (jobName.equals("Necromancer")) {
-        if (gamePlayer.getLife()) {
-          return "この人は生きています";
+    }
+
+    public String getJobName() {
+        if (jobName != null) {
+            return jobName;
         } else {
-          if (gamePlayer.getJob().jobName == "Werewolf") {
-            return "この人は人狼です";
-          } else {
-            return "この人は人狼ではありません";
-          }
+            return "";
         }
-      } else {
-        return "あなたの役職は特別な仕事がありません";
-      }
+    }
+
+    public String getJobNameJapanese() {
+        return plugin.config.getString("japanese.jobs." + jobName);
+    }
+
+
+    public String getCamp() {
+        return this.camp;
+    }
+
+    public String Work(GamePlayer gamePlayer) {
+        // if (!isUsed) {
+        // isUsed = true;
+        if (jobName == "Prophet") {
+            if (gamePlayer.getLife()) {
+                if (gamePlayer.getJob().jobName == "Werewolf") {
+                    return "この人は人狼です";
+                } else {
+                    return "この人は人狼ではありません";
+                }
+            } else {
+                return "この人は死んでいます";
+            }
+        } else if (jobName.equals("Necromancer")) {
+            if (gamePlayer.getLife()) {
+                return "この人は生きています";
+            } else {
+                if (gamePlayer.getJob().jobName == "Werewolf") {
+                    return "この人は人狼です";
+                } else {
+                    return "この人は人狼ではありません";
+                }
+            }
+        } else {
+            return "あなたの役職は特別な仕事がありません";
+        }
+    /*
     }else{
-      return "もう仕事はできません";
+        return "もう仕事はできません";
     }
-  }
+
+     */
+    }
 }
 // 特殊効果
 
