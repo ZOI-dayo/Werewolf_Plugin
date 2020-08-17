d39aa65357feb43b6adac1cb9ffda43103667ca9
zoi <zoi@zoizoi.net>
2020-08-17 17:32:32 +0900

    人狼の痕跡デバッグ

--- src/main/java/net/zoizoi/plugin/werewolf/System/PluginEventsListener.java
+++ src/main/java/net/zoizoi/plugin/werewolf/System/PluginEventsListener.java
@@ -29,9 +29,9 @@ public class PluginEventsListener implements Listener {
 
     @EventHandler
     public void FoodLevelChangeEvent(FoodLevelChangeEvent e) {
-        if(e.getEntity() instanceof Player){
+        if (e.getEntity() instanceof Player) {
             Player player = (Player) e.getEntity();
-            if(SubCommandMaster.gameManager.getGame(SubCommandMaster.GameID).getPlayers().containsKey(player)){
+            if (SubCommandMaster.gameManager.getGame(SubCommandMaster.GameID).getPlayers().containsKey(player)) {
                 e.setCancelled(true);
             }
         }
@@ -42,16 +42,28 @@ public class PluginEventsListener implements Listener {
     public void onPlayerInteract(PlayerInteractEvent e) {
         Player p = e.getPlayer();
         ItemStack item = p.getInventory().getItemInMainHand();
+        plugin.getLogger().info("PluginIventsListener: 45");
         // コンパスを持っているなら{}の中を実行する
+        plugin.getLogger().info("PluginIventsListener: 47: " + item.getType().name());
         if (item.getType() == Material.COMPASS) {
+            plugin.getLogger().info("PluginIventsListener: 48");
             // そのウサギの皮の名前が"人狼の痕跡"なら{}の中を実行する
-            if (item.getItemMeta().getDisplayName() == "人狼の痕跡") {
+            plugin.getLogger().info("PluginIventsListener: 51: " + item.getItemMeta().getDisplayName());
+            if (item.getItemMeta().getDisplayName().equals("人狼の痕跡")) {
+                plugin.getLogger().info("PluginIventsListener: 51");
                 // 右クリックのイベントの場合(左クリックなどを除く)
+                plugin.getLogger().info("PluginIventsListener: 53");
+                plugin.getLogger().info("PluginIventsListener: 56: " + e.getAction().name());
                 if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                    plugin.getLogger().info("PluginIventsListener: 55");
                     // つまり人狼の痕跡を持ち、右クリックされたとき
                     GameManager gameManager = SubCommandMaster.gameManager;
                     for (GamePlayer gamePlayer : gameManager.getGame(SubCommandMaster.GameID).getPlayers().values()) {
+                        plugin.getLogger().info("PluginIventsListener: 62: " + gamePlayer.getPlayer().getName());
+                        plugin.getLogger().info("PluginIventsListener: 59");
+                        plugin.getLogger().info("PluginIventsListener: 64: " + gamePlayer.getJob().getJobName());
                         if (gamePlayer.getJob().getJobName() == "WereWolf") {
+                            plugin.getLogger().info("PluginIventsListener: 61");
                             /*
                             p.sendMessage(gamePlayer.getPlayer().getDisplayName() + "さんは人狼です!");
                             p.getInventory().setItemInMainHand(null);
