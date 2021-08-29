9627db13c06232da67979b24a15fe78404be373a
BuildTools <unconfigured@null.spigotmc.org>
2021-08-29 22:55:46 +0900

    fix spelling shuttle randomItems

--- src/main/java/net/zoizoi/plugin/werewolf/Command/SubCommand/SubCommands/Start/StartItems.java
+++ src/main/java/net/zoizoi/plugin/werewolf/Command/SubCommand/SubCommands/Start/StartItems.java
@@ -51,27 +51,27 @@ public class StartItems {
             inventory.addItem(Steak);
         }
         // ランダムアイテム配布
-        List<ItemStack> RundomItems = new ArrayList<ItemStack>();
+        List<ItemStack> RandomItems = new ArrayList<ItemStack>();
         // ちくわ
         ItemStack Tikuwa = new ItemStack(Material.STICK, 1);
         ItemMeta TikuwaMeta = Tikuwa.getItemMeta();
         TikuwaMeta.setDisplayName("ちくわ");
         Tikuwa.setItemMeta(TikuwaMeta); // Set Meta
-        RundomItems.add(Tikuwa);
+        RandomItems.add(Tikuwa);
         // 跳躍5ポーション
         ItemStack Potion_JUMP_5 = new ItemStack(Material.POTION, 1);
         PotionMeta Potion_JUMP_5_Meta = (PotionMeta) Potion_JUMP_5.getItemMeta();
         Potion_JUMP_5_Meta.setDisplayName("跳躍力上昇のポーション");
         Potion_JUMP_5_Meta.addCustomEffect(new PotionEffect(PotionEffectType.JUMP, 600, 4), true);
         Potion_JUMP_5.setItemMeta(Potion_JUMP_5_Meta); // Set Meta
-        RundomItems.add(Potion_JUMP_5);
+        RandomItems.add(Potion_JUMP_5);
         // 移動速度上昇5
         ItemStack Potion_SPEED_5 = new ItemStack(Material.POTION, 1);
         PotionMeta Potion_SPEED_5_Meta = (PotionMeta) Potion_SPEED_5.getItemMeta();
         Potion_SPEED_5_Meta.setDisplayName("移動速度上昇のポーション");
         Potion_SPEED_5_Meta.addCustomEffect(new PotionEffect(PotionEffectType.SPEED, 600, 4), true);
         Potion_SPEED_5.setItemMeta(Potion_SPEED_5_Meta); // Set Meta
-        RundomItems.add(Potion_SPEED_5);
+        RandomItems.add(Potion_SPEED_5);
         // 一撃必殺ピッケル
         ItemStack SuperPickaxe = new ItemStack(Material.STONE_PICKAXE, 1);
         SuperPickaxe.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 10000);
@@ -81,38 +81,40 @@ public class StartItems {
             ((Damageable) SuperPickaxeMeta).setDamage(131);
         }
         SuperPickaxe.setItemMeta(SuperPickaxeMeta); // Set Meta
-        RundomItems.add(SuperPickaxe);
+        RandomItems.add(SuperPickaxe);
         // 不死のトーテム
         ItemStack Totem = new ItemStack(Material.TOTEM_OF_UNDYING, 1);
-        RundomItems.add(Totem);
+        RandomItems.add(Totem);
         // 透明ポーション
         ItemStack Potion_Invisibility = new ItemStack(Material.POTION, 1);
         PotionMeta Potion_Invisibility_Meta = (PotionMeta) Potion_Invisibility.getItemMeta();
         Potion_Invisibility_Meta.setDisplayName("透明化のポーション");
         Potion_Invisibility_Meta.addCustomEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 600, 1), true);
         Potion_Invisibility.setItemMeta(Potion_Invisibility_Meta); // Set Meta
-        RundomItems.add(Potion_Invisibility);
+        RandomItems.add(Potion_Invisibility);
         // 一撃残留ポーション
         ItemStack Potion_Damage_Lingering = new ItemStack(Material.LINGERING_POTION, 1);
         PotionMeta Potion_Damage_Lingering_Meta = (PotionMeta) Potion_Damage_Lingering.getItemMeta();
         Potion_Damage_Lingering_Meta.setDisplayName("一撃必殺の残留ポーション");
-        Potion_Damage_Lingering_Meta.addCustomEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 200, 1), true);
+        Potion_Damage_Lingering_Meta.addCustomEffect(new PotionEffect(PotionEffectType.HARM, 200, 10000), true);
         Potion_Damage_Lingering.setItemMeta(Potion_Damage_Lingering_Meta); // Set Meta
-        RundomItems.add(Potion_Damage_Lingering);
+        RandomItems.add(Potion_Damage_Lingering);
         // すごいちくわ
         ItemStack SuperTikuwa = new ItemStack(Material.STICK, 1);
         SuperTikuwa.addUnsafeEnchantment(Enchantment.KNOCKBACK, 2);
         ItemMeta SuperTikuwaMeta = SuperTikuwa.getItemMeta();
         SuperTikuwaMeta.setDisplayName("すごいちくわ");
         SuperTikuwa.setItemMeta(SuperTikuwaMeta); // Set Meta
-        RundomItems.add(SuperTikuwa);
+        RandomItems.add(SuperTikuwa);
         // 盲目残留ポーション(x3)
         ItemStack Potion_Blindness_Lingering = new ItemStack(Material.LINGERING_POTION, 3);
         PotionMeta Potion_Blindness_Lingering_Meta = (PotionMeta) Potion_Blindness_Lingering.getItemMeta();
         Potion_Blindness_Lingering_Meta.setDisplayName("盲目の残留ポーション");
         Potion_Blindness_Lingering_Meta.addCustomEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 1), true);
         Potion_Blindness_Lingering.setItemMeta(Potion_Blindness_Lingering_Meta); // Set Meta
-        RundomItems.add(Potion_Blindness_Lingering);
+        RandomItems.add(Potion_Blindness_Lingering);
+	// ランダムアイテムのシャッフル
+        Collections.shuffle(RandomItems);
         //
         List<Player> shuffledPlayerList = new ArrayList<>(/*Arrays.asList((Player[]) gameManager.getGame(GameID).getPlayers().keySet().stream().map(uuid -> plugin.getServer().getPlayer(uuid)).toArray())*/); //TODO ERROR
         for (UUID uuid : gameManager.getGame(GameID).getPlayers().keySet()) {
@@ -134,8 +136,8 @@ java.lang.ClassCastException: [Ljava.lang.Object; cannot be cast to [Lorg.bukkit
          */
         Collections.shuffle(shuffledPlayerList);
         for (int i = 0; i < shuffledPlayerList.size(); i++) {
-            shuffledPlayerList.get(i).getInventory().addItem(RundomItems.get(i));
-            // player.sendMessage("i : " + i + " , " + shuffledPlayerList.get(i).getName() + "に" + RundomItems.get(i) + "を与えました");
+            shuffledPlayerList.get(i).getInventory().addItem(RandomItems.get(i));
+            // player.sendMessage("i : " + i + " , " + shuffledPlayerList.get(i).getName() + "に" + RandomItems.get(i) + "を与えました");
         }
         //
         for (UUID uuid : gameManager.getGame(GameID).getPlayers().keySet()) {
