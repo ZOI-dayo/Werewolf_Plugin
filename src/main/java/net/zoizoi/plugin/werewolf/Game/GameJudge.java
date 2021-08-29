bd22f784c36a62754c0b3a65f5beb2fdb3489c4a
BuildTools <unconfigured@null.spigotmc.org>
2021-08-29 23:27:56 +0900

    fix result report

--- src/main/java/net/zoizoi/plugin/werewolf/Game/GameJudge.java
+++ src/main/java/net/zoizoi/plugin/werewolf/Game/GameJudge.java
@@ -42,14 +42,12 @@ public class GameJudge{
           player.sendMessage("| " + PluginConfig.config.getString("japanese.camp." + gameManager.getGame(GameID).getResult()) + "の勝利 |");
           player.sendMessage("+-----------+");
           player.sendMessage("");
-//          player.sendMessage("今回の役職配分");
-//          player.sendMessage("");
-//          for (UUID uuid1 : gameManager.getGame(GameID).getPlayers().keySet()) {
-//            Player keyPlayer = plugin.getServer().getPlayer(uuid);
-//            GamePlayer gamePlayer = gameManager.getGame(GameID).getPlayers().get(keyPlayer);
-//            player.sendMessage(keyPlayer.getName() + " : " + gamePlayer.getJob().getJobNameJapanese());
-//          }
-//          player.sendMessage("");
+          player.sendMessage("今回の役職配分");
+          player.sendMessage("");
+          for (UUID uuid1 : gameManager.getGame(GameID).getPlayers().keySet()) {
+	      player.sendMessage(plugin.getServer().getPlayer(uuid).getName() + " : " + gameManager.getGame(GameID).getPlayers().get(uuid1).getJob().getJobNameJapanese());
+          }
+          player.sendMessage("");
           // 花火を打ち上げる
           /*
           for (int i = 0; i < 10; i++) {
@@ -79,16 +77,7 @@ public class GameJudge{
         death.setGameMode(GameMode.SPECTATOR);
         death.sendMessage("あなたは死にました");
         death.sendMessage("DiscordのVCを切ってください");
-        death.sendMessage("今回の役職配分");
-        death.sendMessage("");
-        for (UUID uuid1 : gameManager.getGame(GameID).getPlayers().keySet()) {
-          Player keyPlayer = plugin.getServer().getPlayer(uuid1);
-          GamePlayer gamePlayer = gameManager.getGame(GameID).getPlayers().get(death.getUniqueId());
-          death.sendMessage(keyPlayer.getName() + " : " + gamePlayer.getJob().getJobNameJapanese());
-        }
-        death.sendMessage("");
       }
     }
   }
 }
-
